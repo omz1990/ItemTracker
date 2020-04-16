778141f00ca3e3ea98245d318888d507aa429043
Omar Mujtaba <omar.mujtaba@gmail.com>
2020-04-16 22:51:50 +1000

    Add create account UI

---
+++ ItemTracker/Controller/SignInViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignInViewController.swift
+//  ItemTracker
+//
+//  Created by Omar Mujtaba on 16/4/20.
+//  Copyright © 2020 AmmoLogic Training. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ ItemTracker/Controller/SignUpViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpViewController.swift
+//  ItemTracker
+//
+//  Created by Omar Mujtaba on 16/4/20.
+//  Copyright © 2020 AmmoLogic Training. All rights reserved.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
