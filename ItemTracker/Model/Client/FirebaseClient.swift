058fe1731218cdcd5f05e6b5189f40ffac2e0be0
Omar Mujtaba <omar.mujtaba@gmail.com>
2020-04-19 19:00:18 +1000

    Parse storages data to local struct from Firebase

--- ItemTracker/Model/Client/FirebaseClient.swift
+++ ItemTracker/Model/Client/FirebaseClient.swift
@@ -66,6 +66,18 @@ class FirebaseClient {
         if let uid = Auth.auth().currentUser?.uid {
             let dbRef = Database.database().reference()
             getLocationsList(uid: uid, dbRef: dbRef, completion: completion)
+            getLocationsList(uid: uid, dbRef: dbRef) { (locations, error) in
+                guard let locations = locations else {
+                    completion(nil, error)
+                    return
+                }
+                
+                locations.forEach { (location) in
+                    getStoragesList(uid: uid, locationId: location.id, dbRef: dbRef) { (storages, error) in
+                        print("Storages: \(storages)")
+                    }
+                }
+            }
         } else {
             completion(nil, nil)
         }
@@ -92,6 +104,7 @@ class FirebaseClient {
                 let updatedAt: Date = Date(timeIntervalSince1970: updatedAtTimestamp)
                 
                 let location = Location(id: locationId, name: name, subName: subName, description: description, imageUrl: imageUrl, type: type, subType: subType, tags: tags, createdAt: createdAt, updatedAt: updatedAt, storages: nil)
+                
                 locations.append(location)
             })
             completion(locations, nil)
@@ -99,7 +112,31 @@ class FirebaseClient {
     }
     
     class func getStoragesList(uid: String, locationId: String, dbRef: DatabaseReference, completion: @escaping ([Storage]?, Error?) -> Void) {
+        let path = "\(DatabaseCollection.storage(uid: uid).path)/\(locationId)"
         
+        dbRef.child(path).observeSingleEvent(of: .value) { (snapshot) in
+            let storagesDict = snapshot.value as? [String: AnyObject]
+            var storages: [Storage] = []
+            
+            storagesDict?.forEach({ (arg0) in
+                let(storageId, storageObject) = arg0
+                let locationId: String = storageObject.value(forKey: DatabaseField.Common.locationId) as? String ?? ""
+                let name: String = storageObject.value(forKey: DatabaseField.Common.name) as? String ?? ""
+                let description: String? = storageObject.value(forKey: DatabaseField.Common.description) as? String
+                let imageUrl: String? = storageObject.value(forKey: DatabaseField.Common.imageUrl) as? String
+                let type: String = storageObject.value(forKey: DatabaseField.Common.type) as? String ?? ""
+                let tags: [String]? = storageObject.value(forKey: DatabaseField.Common.tags) as? [String]
+                let createdAtTimestamp: Double = storageObject.value(forKey: DatabaseField.Common.createdAt) as? Double ?? 0
+                let createdAt: Date = Date(timeIntervalSince1970: createdAtTimestamp)
+                let updatedAtTimestamp: Double = storageObject.value(forKey: DatabaseField.Common.updatedAt) as? Double ?? 0
+                let updatedAt: Date = Date(timeIntervalSince1970: updatedAtTimestamp)
+                
+                let storage = Storage(id: storageId, locationId: locationId, name: name, description: description, imageUrl: imageUrl, type: type, tags: tags, createdAt: createdAt, updatedAt: updatedAt, items: nil)
+                
+                storages.append(storage)
+            })
+            completion(storages, nil)
+        }
     }
     
     class func getItemsList(uid: String, locationId: String, storageId: String, dbRef: DatabaseReference, completion: @escaping ([Item]?, Error?) -> Void) {
